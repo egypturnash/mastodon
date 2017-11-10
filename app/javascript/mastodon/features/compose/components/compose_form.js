b9586c79629756dd0332b17f74a3bd13dae39a3f
Margaret Trauth <egypt@urnash.com>
2017-11-09 18:03:03 -0800

    Initial commit: adds support for 7777-character toots, with the caveat that anything longer than 777 characters must have a CW.

--- app/javascript/mastodon/features/compose/components/compose_form.js
+++ app/javascript/mastodon/features/compose/components/compose_form.js
@@ -146,6 +146,7 @@ export default class ComposeForm extends ImmutablePureComponent {
     const { intl, onPaste, showSearch } = this.props;
     const disabled = this.props.is_submitting;
     const text     = [this.props.spoiler_text, countableText(this.props.text)].join('');
+    const maxTootLength = 7777;
 
     let publishText = '';
 
@@ -202,8 +203,8 @@ export default class ComposeForm extends ImmutablePureComponent {
           </div>
 
           <div className='compose-form__publish'>
-            <div className='character-counter__wrapper'><CharacterCounter max={500} text={text} /></div>
-            <div className='compose-form__publish-button-wrapper'><Button text={publishText} onClick={this.handleSubmit} disabled={disabled || this.props.is_uploading || length(text) > 500 || (text.length !== 0 && text.trim().length === 0)} block /></div>
+            <div className='character-counter__wrapper'><CharacterCounter max={maxTootLength} text={text} /></div>
+            <div className='compose-form__publish-button-wrapper'><Button text={publishText} onClick={this.handleSubmit} disabled={disabled || this.props.is_uploading || length(text) > maxTootLength || (text.length !== 0 && text.trim().length === 0)} block /></div>
           </div>
         </div>
       </div>
